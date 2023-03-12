81b6f64c3a5b8b0c0c4b5db70e703c0ef2c2cbb2
minhphan46 <21522345@gm.uit.edu.vn>
2023-03-12 17:34:18 +0700

    done app

--- app/src/main/java/com/example/todo_app/MainActivity.java
+++ app/src/main/java/com/example/todo_app/MainActivity.java
@@ -1,23 +1,32 @@
 package com.example.todo_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.todo_app.Adapter.ToDoAdapter;
 import com.example.todo_app.Models.ToDoModel;
+import com.example.todo_app.utils.DatabaseHandler;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DialogCloseListener{
 
     private RecyclerView tasksRecyclerView;
     private ToDoAdapter tasksAdapter;
+    private FloatingActionButton fab;
 
     private List<ToDoModel> taskList;
+    private DatabaseHandler db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,23 +34,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
+        db = new DatabaseHandler(this);
+        db.openDatabase();
+
+
         // set default
         taskList = new ArrayList<>();
         tasksRecyclerView = findViewById(R.id.tasksRecyclerView);
         tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        tasksAdapter = new ToDoAdapter(this);
+        tasksAdapter = new ToDoAdapter(db,this);
         tasksRecyclerView.setAdapter(tasksAdapter);
 
-        // dummy data
-        ToDoModel task = new ToDoModel();
-        task.setTask("This is a test Task");
-        task.setId(1);
-        task.setStatus(0);
-        // add dummy data to list
-        for(int i = 0; i < 5;i ++){
-            taskList.add(task);
-        }
-        // update tasks
+        // floating actionButton
+        fab = findViewById(R.id.fab);
+
+        //
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new RecyclerItemTouchHeper(tasksAdapter));
+        itemTouchHelper.attachToRecyclerView(tasksRecyclerView);
+        // get task from database
+        taskList = db.getAllTasks();
+        Collections.reverse(taskList);
         tasksAdapter.setTasks(taskList);
+
+        fab.setOnClickListener(v -> {
+            AddNewTask.newInstance().show(getSupportFragmentManager(), AddNewTask.TAG);
+        });
+    }
+
+    @Override
+    public void handleDialogClose(DialogInterface dialog) {
+        taskList = db.getAllTasks();
+        Collections.reverse(taskList);
+        tasksAdapter.setTasks(taskList);
+        tasksAdapter.notifyDataSetChanged();
     }
 }
